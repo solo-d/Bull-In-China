9356248a3c42783f615c1b624a6ef4445ba12706
David S <desboy13@hotmail.com>
2020-02-07 00:59:04 -0500

    Updated scripts

--- Bull In A China Shop/Assets/Scripts/BullScript.cs
+++ Bull In A China Shop/Assets/Scripts/BullScript.cs
@@ -6,6 +6,9 @@ using Random = UnityEngine.Random;
 
 public partial class BullScript : MonoBehaviour
 {
+    private Vector3 startPosition = new Vector3(8.74f, 0.69f, 1.55f);
+
+    private IEnumerator bullMovement_coroutine;
 
     private bool dazed;
 
@@ -16,13 +19,14 @@ public partial class BullScript : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
 
-        var collidedWith = collision.gameObject;
+        var collidedWith = collision.gameObject; // Get the object that the bull collided with
         
         if (collidedWith.tag == "Floor") return;
         if (collidedWith.tag == "Wall")
-        {
-            
+        {            
             BullStop();
+            ChangeRotation("");
+
         };
         if (collidedWith.tag == "Shelf") return;
         if (collidedWith.tag == "Trap") return;
@@ -35,28 +39,52 @@ public partial class BullScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        Vector3 levelSize = GameObject.Find("Terrain").GetComponent<Terrain>().terrainData.size;
-		float playingField = (levelSize.x * levelSize.z);
-		moveSpeed = (playingField)/60;
+        BullInit();
     }
 
     void Update()
     {
-        var delta = Time.deltaTime;
-        if(dazed) return;
-        if (seeking)
-        {
-            this.Seek(delta);
-            return;
-        }
-        Move(Time.deltaTime);
-        this.transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, this.transform.rotation.w);
+        //var delta = Time.deltaTime;
+        //if(dazed) return;
+        //if (seeking)
+        //{
+        //    Debug.Log(delta);
+        //    this.Seek(delta);
+        //    return;
+        //}
+    }
+
+    // Whatever starts the level should call 
+    public void BullInit()
+    {
+        ToggleDoors(true);
+
+        // We want the bull to start at the doorway
+        this.transform.position = startPosition;
+
+        // We will calculate the bull running speedy based on the size of the man divided by 30 seconds. 
+        // The bull should be able travel from one end of the map to teh other in 30 seconds
+        Vector3 levelSize = GameObject.Find("Terrain").GetComponent<Terrain>().terrainData.size;
+        float playingField = (levelSize.x * levelSize.z);
+        moveSpeed = (playingField) / 30;
+        BullRun();
+        ToggleDoors(false);
+    }
+
+    private void ToggleDoors(bool showDoors) 
+    {
+        GameObject doors = GameObject.FindWithTag("Door");
+        doors.SetActive(showDoors);
+    }
+    public void BullRun()
+    {
+        bullMovement_coroutine = MoveBull();
+        StartCoroutine(bullMovement_coroutine);
     }
 
     public void BullStop()
     {
-        
+        StopCoroutine(bullMovement_coroutine);
         var animator = GetComponent<Animator>();
         this.dazed = true;
         animator.SetBool("Collided", true);
@@ -68,8 +96,7 @@ public partial class BullScript : MonoBehaviour
         this.dazed = false;
         this.seeking = true;
         var animator = GetComponent<Animator>();
-        animator.SetBool("Collided", false);
-        
+        animator.SetBool("Collided", false);        
     }
 
     public void Seek(float deltaTime)
@@ -80,8 +107,6 @@ public partial class BullScript : MonoBehaviour
             var animator = GetComponent<Animator>();
             animator.SetBool("Reset", true);
         }
-        
-
     }
 
     /// <summary>
@@ -94,8 +119,29 @@ public partial class BullScript : MonoBehaviour
 
 	public void ChangeStamina(int changeStaminaBy) { }
 
-	public void ChangeRotation(string turnDirection) { }
+	public void ChangeRotation(string turnDirection) {
+        for (int i = Random.Range(0, 10); i > 0; i--)
+        {
+            this.Seek(0.1f);
+        }
+        this.seeking = false;
+        var animator = GetComponent<Animator>();
+        animator.SetBool("Reset", true);
 
+        BullRun();
+    }
+
+
+    //This is a coroutine that moves the bull
+    IEnumerator MoveBull()
+    {
+        for (; ; )
+        {
+            Move(Time.deltaTime);
+            this.transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, this.transform.rotation.w);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 
     void Move(float timeDelta)
 	{
