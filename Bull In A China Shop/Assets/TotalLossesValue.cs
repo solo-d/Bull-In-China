2f714a374da9cd3d0f5ad5e44cde2be9002f0107
dofsz <50952165+dofsz@users.noreply.github.com>
2020-02-12 22:36:22 -0500

    Updated with Total Losess on Results Screen

---
+++ Bull In A China Shop/Assets/TotalLossesValue.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TotalLossesValue : MonoBehaviour
+{
+
+    public Canvas myCanvas;
+    public Text totalScoretext;
+
+
+    private string display = "";
+
+
+
+    List<string> addupValues;
+
+    private bool callMe;
+
+
+    void Start()
+    {
+        addupValues = new List<string>();
+
+        addupValues.Add("Total Losses Variable");
+
+
+
+        callMe = true;
+
+    }
+
+    void Update()
+    {
+        if (callMe)
+        {
+            AddText();
+            callMe = false;
+        }
+    }
+
+
+
+    void AddText()
+    {
+        foreach (string msg in addupValues)
+        {
+            display = display.ToString() + msg.ToString() + "\n";
+        }
+        totalScoretext.text = display;
+    }
+
+}
