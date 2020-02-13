801815046ef7c1c861948856fdff6cb373131604
dofsz <50952165+dofsz@users.noreply.github.com>
2020-02-12 21:20:39 -0500

    Added Results scene when round ends

---
+++ Bull In A China Shop/Assets/DisplayText.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+
+    public Canvas myCanvas;
+    public Text lossesText;
+
+    private string display = "";
+
+    List<string> chinaDestroyed;
+
+    private bool callMe;
+
+
+    void Start()
+    {
+        chinaDestroyed = new List<string>();
+
+        chinaDestroyed.Add("Big Plates Variable");
+        chinaDestroyed.Add("Bowls Variable ");
+        chinaDestroyed.Add("Sets of Plates Variable ");
+        chinaDestroyed.Add("Small Plates Variable ");
+        chinaDestroyed.Add("Tea Cups Variable ");
+        chinaDestroyed.Add("Tea Kettles Variable ");
+        chinaDestroyed.Add("Vases Variable ");
+      
+        callMe = true;
+
+    }
+
+    void Update()
+    {
+        if (callMe)
+        {
+            AddText();
+            callMe = false;
+        }
+    }
+
+    void AddText()
+    {
+        foreach (string msg in chinaDestroyed)
+        {
+            display = display.ToString() + msg.ToString() + "\n";
+        }
+        lossesText.text = display;
+    }
+}
